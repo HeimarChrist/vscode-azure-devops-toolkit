5132b24afc697f8e51bd613e1f26d6f38f74eeed
Heimar <hchrist001@gmail.com>
2025-02-10 07:10:33 +0100

    feat: tree provider first steps

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,19 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { AzureDevopsProvider, Repos } from './providers/azureDevopsProvider';
+import { MicrosoftProviderId, MicrosoftScopes } from './Utils/Constants';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "azure-devops-toolkit" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('azure-devops-toolkit.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Azure DevOps Toolkit!');
-	});
+export async function activate(context: vscode.ExtensionContext) {
 
-	context.subscriptions.push(disposable);
+	let session: vscode.AuthenticationSession | undefined = await vscode.authentication.getSession(MicrosoftProviderId, MicrosoftScopes, { createIfNone: true });
+	const azureDevopsProvider = new AzureDevopsProvider(session);
+	const treeView = vscode.window.createTreeView('devops-toolkit-view', { treeDataProvider: azureDevopsProvider });
+	context.subscriptions.push(treeView);
+	context.subscriptions.push(vscode.commands.registerCommand('devops-toolkit.clone', async (item : Repos) => { vscode.commands.executeCommand('git.clone', item.repoUrl); }));
+	context.subscriptions.push(vscode.commands.registerCommand('devops-toolkit.refresh', async (item) => { azureDevopsProvider.refresh(item); }));
 }
 
 // This method is called when your extension is deactivated
